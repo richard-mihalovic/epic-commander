6b6386004da175b5a846fd7e05cfffd0f4d439a7
Richard Mihalovič <richard.mihalovic@gmail.com>
2016-06-25 23:35:18 +0200

    Extended previev panel functionality (text & audio).

--- src/components/PreviewPanel.js
+++ src/components/PreviewPanel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { findIndex } from 'lodash';
+import showdown from 'showdown';
 
 import Container from './Container';
 import Row from './Row';
@@ -22,21 +23,76 @@ class PreviewPanel extends React.Component {
 
         return (
             <Container className="preview_panel">
-                { previewIsDir ? 
-                    <div className="no_preview_available">No preview available.</div> 
-                    : 
-                    this.previewImage(previewFullPath) 
+                { previewIsDir ?
+                    <div className="no_preview_available">No preview available.</div>
+                    :
+                    this.preview(previewPath, previewRecord, previewFullPath)
                 }
             </Container>
         )
     }
 
+    preview(previewPath, previewRecord, previewFullPath) {
+        let [wasProcessed, element] = [undefined, undefined];
+
+        [wasProcessed, element] = this.previewImage(previewFullPath);
+        if (wasProcessed) return element;
+
+        [wasProcessed, element] = this.previewAudio(previewFullPath);
+        if (wasProcessed) return element;
+
+        [wasProcessed, element] = this.previewFile(previewFullPath);
+        if (wasProcessed) return element;
+
+        return <div className="no_preview_available">No preview available.</div>
+    }
+
     previewImage(filename) {
-        if(/\.(jpe?g|png|gif|bmp)$/i.test(filename)) {
-            return (
+        if (/\.(jpe?g|png|gif|bmp)$/i.test(filename)) {
+            return [true, (
                 <img style={{ maxWidth: '100%' }} src={ 'file://' + filename } />
-            )
+            )];
         }
+
+        return [false, null];
+    }
+
+    previewFile(filename) {
+        if (filename.endsWith('.md')) {
+            const content = FileUtils.readFile(filename);
+            let converter = new showdown.Converter();
+            let html = converter.makeHtml(content);
+            return [ true, <p style={{ padding: '1em', color: 'white' }} dangerouslySetInnerHTML={ { __html: html } } /> ];
+        } else if (/\.(txt|srt)$/i.test(filename)) {
+            const content = FileUtils.readFile(filename);
+            return [true, (
+                <p style={{ whiteSpace: 'pre', padding: '1em', color: 'white' }}>{ content }</p>
+            )];
+        }
+
+        return [false, null];
+    }
+
+    previewAudio(filename) {
+        if (/\.(mp3|ogg|wav)$/i.test(filename)) {
+            let mediaType = '';
+
+            if (filename.endsWith('.mp3')) {
+                mediaType = 'audio/mpeg';
+            } else if (filename.endsWith('.ogg')) {
+                mediaType = 'audio/ogg';
+            } else if (filename.endsWith('.wav')) {
+                mediaType = 'audio/wav';
+            }
+
+            return [true, (
+                <audio controls>
+                    <source src={ 'file://' + filename } type={mediaType} />
+                </audio>
+            )];
+        }
+
+        return [false, null];
     }
 }
 
